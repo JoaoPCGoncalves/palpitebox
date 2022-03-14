cadc14e11882c58e426ca561b94f864fef7583fa
João P. C. Gonçalves <jpocastilho@gmail.com>
2022-03-14 10:12:15 -0300

    Initial version

---
+++ components/PageTitle.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Head from 'next/head'
+
+const PageTitle = ({ Title }) => {
+return (
+    <head>
+        <title>{Title} - PalpiteBox</title>
+    </head>
+    )    
+}
+
+export default PageTitle
