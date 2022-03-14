301ea5ec7df1480dc40a023a03e31789d9399870
João P. C. Gonçalves <jpocastilho@gmail.com>
2022-03-14 16:10:59 -0300

    Added Whatsapp button

--- pages/contato.js
+++ pages/contato.js
@@ -6,12 +6,12 @@ const Contato = () => {
     return(
         <div>
             <PageTitle Title='Contato' />
-            <h1>Contato</h1>
-            <div>
-                <Link href='/'>
-                    <a>Home</a>
-                </Link>
-            </div>
+            <h1 className='text-center px-12 py-4 font-bold text-xl'>Converse com a gente no WhatsApp</h1>
+            <div className='text-center px-12 py-4'>
+            <button className='text-center bg-green-600 px-12 py-4 rounded-lg shadow-lg hover:shadow'>
+                <a className='text-white' href="https://web.whatsapp.com/send?phone=5511900000000" title="Compartilhe no WhatsApp">INICIAR CONVERSA</a>
+            </button>
+            </div>            
         </div>
     )
 }
